fa27f5f7b6c566847c3e0d2f181b68c257f4e14c
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-03 10:38:39 -0600

    user can create a cart

--- resources/carts.py
+++ resources/carts.py
@@ -9,18 +9,23 @@ from playhouse.shortcuts import model_to_dict
 # blueprint
 carts = Blueprint('carts', 'carts')
 
-# # create cart route
-# # the cart gets created when the user adds an item
-# @carts.route('/', methods=["POST"])
+# create cart route
+# the cart gets created when the user adds an item
+@carts.route('/', methods=["POST"])
 
-# # the user must be logged in to add things to their cart
-# @login_required
+# the user must be logged in to add things to their cart
+@login_required
 
-# def create_cart():
-#     # get the payload
-#     payload = request.get_json()
+def create_cart():
 
-#     # create the cart
-#     cart = models.Cart.create(quantity=payload['quantity'], product_id=payload['product_id'], paid=payload['paid'], user_id=payload['user_id'])
+    # get the payload
+    payload = request.get_json()
 
-#     print(model_to_dict(cart), "model_to_of_created_product")
+    # create the cart
+    cart = models.Cart.create(quantity=payload['quantity'], product_id=payload['product_id'], paid=payload['paid'], user_id=payload['user_id'])
+
+    print(model_to_dict(cart), "model_to_of_created_product")
+    cart_dict = model_to_dict(cart)
+
+    # return success
+    return jsonify(data=cart_dict, status={"code": 201, "message": "You successfully created your cart"})
