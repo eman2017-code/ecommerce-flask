2b440a8b8a9adac39c9cca3c540c06b1c78a2509
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-04 11:45:33 -0600

    added pseudocode to how to checkout a user

--- resources/cartItems.py
+++ resources/cartItems.py
@@ -8,3 +8,13 @@ from playhouse.shortcuts import model_to_dict
 
 # blueprint
 cartItems = Blueprint('cartItems', 'cartItems')
+
+# checkout route
+@cartItems.route('/checkout', methods=["POST"])
+# the user must be logged in
+@login_required
+def checkout():
+    # grab the cart that belongs to the user
+    # list out all the items that are in the cart
+    # list out the quantity that is in their cart
+    # add up all the prices that each item has
